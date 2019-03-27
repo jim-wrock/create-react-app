4c3d630a494a71f7ba80eae8af3d3d70c8ef64fb
Jim Decocq <jim.decocq@gmail.com>
2019-03-27 17:52:54 +0100

    atelier 2 ok

---
+++ src/Hello.jsx
@@ -0,0 +1,9 @@
+
+import React from "react";
+
+const Hello = () => <div>
+
+    <h1> Hello React World</h1>
+    </div>
+    
+export default Hello;
---
+++ src/avatar.jsx
@@ -0,0 +1,6 @@
+const Avatar = props => (
+    <div>
+        <h3>{props.name}</h3>
+        <img src={props.avatar} alt=""/>
+    </div>
+);
